6e25bd6e937023adc0fa3b21e4aa54e0ec3189b8
christianatzeni <christian.alessandro.atzeni@it.ey.com>
2025-10-27 11:56:09 +0100

    Fixed sectors

--- src/app/startup/[id]/page.tsx
+++ src/app/startup/[id]/page.tsx
@@ -54,9 +54,6 @@ export default function StartupDetails({ params }: { params: { id: string } }) {
                             <Stack direction="row" spacing={0} mt={1} flexWrap="wrap">
                                 {startup?.sectors
                                     ?.split(",")
-                                    ?.map((word) =>
-                                        word.length === 1 ? word.toLowerCase() : word[0].toUpperCase() + word.slice(1).toLowerCase()
-                                    )
                                     ?.map((sector, index) => (
                                         <Chip
                                             key={index}
